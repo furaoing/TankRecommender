429f16c34c8d62e7075f3bfb2c90c3ef856c05a5
furaoing <furaoing@hotmail.com>
2016-04-29 17:57:36 +0800

    updates

--- nlp_client.py
+++ nlp_client.py
@@ -8,6 +8,7 @@ from thriftpy.rpc import client_context
 from config import HANLPRPC_HOST
 from config import HANLPRPC_PORT
 from waffle import system
+import time
 
 rel_path = r"hanlprpc.thrift"
 abs_path = system.create_abs_path(rel_path)
@@ -21,6 +22,7 @@ def rpc_seg(text):
                         HANLPRPC_HOST, HANLPRPC_PORT) as client:
 
         result = client.seg(text)
+    time.sleep(0.2)
     return result
 
 if __name__ == '__main__':
