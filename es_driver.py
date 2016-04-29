c8e4bb2773012006c7bac83462ac3425919e28d4
furaoing <furaoing@hotmail.com>
2016-04-29 15:27:09 +0800

    updates

--- es_driver.py
+++ es_driver.py
@@ -9,6 +9,7 @@ import json
 import requests
 from waffle import system
 import time
+from retry import retry
 from config import ES_URL
 from config import construct_query
 from config import ES_TANK_TIMEOUT
@@ -16,28 +17,33 @@ from config import ES_BATCH_SIZE
 from config import ERROR
 
 
+@retry(tries=3, delay=2)
+def es_fetch(query):
+    return requests.post(ES_URL,
+                         data=json.dumps(query),
+                         timeout=ES_TANK_TIMEOUT)
+
+
 def pull_news_from_tank(_start_index, _batch_size, bt, et):
-    signal = None
-    url = ES_URL
-    query = construct_query(bt, et, _start_index, _batch_size)
-    timer = system.Timer()
-    r = requests.post(url, data=json.dumps(query), timeout=ES_TANK_TIMEOUT)
-    t = str(timer.end())
-    print("ES Query Time: " + t)
-    obj = json.loads(r.text)
-    batch_size = len(obj["hits"]["hits"])
-    if batch_size < _batch_size:
-        signal = -2
-    else:
-        signal = 0
-    
-    news = list()
-    
+
     def test_void(item_attr):
         if item_attr == "" or item_attr == None or item_attr == " ":
             return False
         else:
             return True
+
+    news = list()
+    query = construct_query(bt, et, _start_index, _batch_size)
+    timer = system.Timer()
+    r = es_fetch(query)
+    t = str(timer.end())
+    print("ES Query Time: " + t)
+    obj = json.loads(r.text)
+    batch_size = len(obj["hits"]["hits"])
+    if batch_size < _batch_size:
+        signal = ERROR.ES_NO_MORE_DATA
+    else:
+        signal = ERROR.ES_HAS_DATA
             
     for item in obj["hits"]["hits"]:
         if "cleanTitle" in item["_source"]:
@@ -56,63 +62,43 @@ def pull_news_from_tank(_start_index, _batch_size, bt, et):
                 break
 
         if test_void(title) and test_void(text) and ig:
-            news.append({"Url": item["_source"]["url"], "Title": title, "Content": text, "iG": ig})
+            news.append({"Url": item["_source"]["url"],
+                         "Title": title,
+                         "Content": text,
+                         "iG": ig})
     return signal, news
 
 
 def fetch_news_from_es(bt, et):
     news = list()
     start_index = 0
-    signal = None
-    news_buffer = None
 
     while True:
         # starting fetching data from es on a batch by batch style
-        for attempt in range(10):
-            try:
-                signal, news_buffer = pull_news_from_tank(start_index,
-                                                          ES_BATCH_SIZE, bt, et)
-                time.sleep(1)
-            except Exception:
-                print("Time Out")
-                print("Try One More Time")
-                time.sleep(5)
-                # sleep for 5 sec and re-try
+        try:
+            signal, news_buffer = pull_news_from_tank(start_index,
+                                                      ES_BATCH_SIZE, bt, et)
 
-                signal = ERROR.PULL_METHOD_FAILED
-                # signal = -3 indicates that pull new method failed
-
-                continue
-                # move to the next attempt
-            time.sleep(0.1)
-            break
-
-        if signal is None:
-            # Terminate execuation is signal is None
-            print("Error, var signal is not assigned value")
-            raise Exception
-
-        if signal == ERROR.ES_NO_MORE_DATA:
-            break
-            # no more batches based on query
-        elif signal == ERROR.PULL_METHOD_FAILED:
-            print("All retry failed, break out of loop")
-            break
-        elif signal == ERROR.ES_HAS_DATA:
-            print("Batch Arrived")
-
-            if news_buffer:
-                news = news + news_buffer
-            else:
+            if signal is None:
+                # Terminate execuation if signal is None
+                print("Error, var signal is not assigned value")
                 raise Exception
-            start_index += ES_BATCH_SIZE
-            # es_batch_size is a global variable in the current scope
 
-            time.sleep(2)
-            # sleep for 2 sec and then fetch the next batch
+            if signal == ERROR.ES_NO_MORE_DATA:
+                break
+                # no more batches based on query
+            elif signal == ERROR.ES_HAS_DATA:
+                print("Batch Arrived")
 
-        else:
-            print("Error, var signal is assigned illegal value")
-            raise Exception
+                news = news + news_buffer
+                start_index += ES_BATCH_SIZE
+                time.sleep(2)
+                # sleep for 2 sec and then fetch the next batch
+            else:
+                print("Error, var signal is assigned illegal value")
+                raise Exception
 
+        except Exception:
+            print("Retries failed, break out of loop, drop ES connection attempts")
+            break
     return news
