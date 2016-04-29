7dfda334edb63aa9e6839be11f30353109b67f80
furaoing <furaoing@hotmail.com>
2016-04-29 16:11:00 +0800

    updates

--- nlp_client.py
+++ nlp_client.py
@@ -7,8 +7,11 @@ import thriftpy
 from thriftpy.rpc import client_context
 from config import HANLPRPC_HOST
 from config import HANLPRPC_PORT
+from waffle import system
 
-hanlprpc_thrift = thriftpy.load(r"hanlprpc.thrift",
+rel_path = r"hanlprpc.thrift"
+abs_path = system.create_abs_path(rel_path)
+hanlprpc_thrift = thriftpy.load(abs_path,
                                 module_name="hanlprpc_thrift")
 
 
